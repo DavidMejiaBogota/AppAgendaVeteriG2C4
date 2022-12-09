10d5743e46a9a95c3cd2d0a01e43d6d816fc6fa6
DavidMejiaBogota <jose.david.mejia.meneses@gmail.com>
2022-12-09 14:55:07 -0500

    Validación del formulario de registro

--- David/FullStackJS/frontend/src/paginas/Registrar.jsx
+++ David/FullStackJS/frontend/src/paginas/Registrar.jsx
@@ -7,7 +7,23 @@ const Resgistrar = () => {
   const [ email, setEmail ] = useState('')
   const [ password, setPassword ] = useState('')
   const [ repetirPassword, setRepetirPassword ] = useState('')
-  
+  const handleSubmit = e => {
+    e.preventDefault(); //para prevenir la acció por default
+    if([nombre, email, password, repetirPassword].includes('')) {
+      console.log('Hay campos vacios');
+      return;
+    }
+
+    if(password !== repetirPassword) {
+      console.log('El password no es igual');
+      return;
+    }
+
+    if(password.length <6) {
+      console.log('El password debe contener mínimo 6 caracteres');
+      return;
+    }
+  }
 
     return (
       <>
@@ -18,7 +34,9 @@ const Resgistrar = () => {
         </div>
 
         <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
-          <form>
+          <form
+            onSubmit={handleSubmit}
+            >
 
             <div className="my-5">
               <label

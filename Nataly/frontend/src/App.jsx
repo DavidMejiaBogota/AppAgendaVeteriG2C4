8191e96751cf9f9218700e54e5a6fb3ed2e2a71b
natalyq1 <onatylili@gmail.com>
2022-12-12 22:29:59 -0500

    Rotes for Layout-Auth

--- Nataly/frontend/src/App.jsx
+++ Nataly/frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import AuthLayout from "./layout/AuthLayout";
+import ConfirmarCuenta from "./paginas/ConfirmarCuenta";
 import Login from "./paginas/Login";
+import OlvidePassword from "./paginas/OlvidePassword";
+import Registrar from "./paginas/Registrar";
 
 function App() {
  
@@ -11,7 +14,9 @@ function App() {
         <Routes>
           <Route path="/" element={<AuthLayout/>}>
             <Route index element={<Login/>}/>
-
+            <Route path="registrar" element={<Registrar/>}/>
+            <Route path="olvide-password" element={<OlvidePassword/>}/>
+            <Route path="confirmar/:id" element={<ConfirmarCuenta/>}/>
           </Route>
         </Routes>
       
---
+++ Nataly/frontend/src/paginas/Registrar.jsx
@@ -0,0 +1,11 @@
+
+
+const Registrar = () => {
+  return (
+    <>
+    <h1>desde Registrar</h1>
+    </>
+  )
+}
+
+export default Registrar

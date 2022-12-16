9bb16c7ebceebee50731d7212b4b89f948a3d2d7
DavidMejiaBogota <jose.david.mejia.meneses@gmail.com>
2022-12-16 15:33:13 -0500

    verificación carga urlBase en axios

--- David/FullStackJS/frontend/src/config/axios.jsx
+++ David/FullStackJS/frontend/src/config/axios.jsx
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const clienteAxios = axios.create({
    baseURL: `${import.meta.env.VITE_BACKEND_URL}/api` //endPoint/dominio principal en el cual se van a hacer todas las peticiones.

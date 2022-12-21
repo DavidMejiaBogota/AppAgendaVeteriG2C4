e44c04aa80252f5bf627b20615a822f9d4e74ce2
otrousuario <edwardusag9@gmail.com>
2022-12-20 23:17:23 -0500

    inicio crea y recupera sesion

--- Cuthbert/FullStack/frontend/src/paginas/Registrar.jsx
+++ Cuthbert/FullStack/frontend/src/paginas/Registrar.jsx
@@ -1,5 +1,37 @@
+import { useState } from "react";
+
 import { Link } from "react-router-dom";
-function Registrar() {
+import Alerta  from "../components/Alerta";
+const Registrar = () => {
+  const [ nombre, setNombre ]= useState(" ");
+  const [ email, setEmail ]= useState(" ");
+  const [ password, setPassword ]= useState(" ");
+  const [ repetirPassword, setRepetirPassword ]= useState(" ");
+
+  const [ alerta, setAlerta ]= useState({});
+
+  const handleSubmit = e =>{
+    e.preventDefault();
+
+    if([nombre, email,password, repetirPassword].includes("")) {
+
+      setAlerta({msg:"Hay campos vacios",error: true})
+      return;
+    }
+    if(password !== repetirPassword){
+     
+      setAlerta({msg:"Los Password no son iguales",error: true})
+      return
+    }
+    if(password < 6){
+     
+      setAlerta({msg:"Los Password es corto, agrega minimo 6 caracteres",error: true})
+      return
+    }
+   
+    
+  }
+
   return (
     <>
       <div>
@@ -9,9 +41,13 @@ function Registrar() {
         </h1>
       </div>
       <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
-
-        <form>
-        <div className="my-5">
+      <Alerta
+      alerta={alerta}
+      />
+        <form
+        onSubmit={handleSubmit}
+        >
+          <div className="my-5">
             <label
               className="uppercase text-gray-600 block text-xl font-bold"
             >
@@ -21,6 +57,8 @@ function Registrar() {
               type="text"
               placeholder="Tu Nombre"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={nombre}
+              onChange={ e => setNombre(e.target.value)}
             />
           </div>
           <div className="my-5">
@@ -33,6 +71,8 @@ function Registrar() {
               type="email"
               placeholder="Email de registro"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={email}
+              onChange={ e => setEmail(e.target.value)}
             />
           </div>
           <div className="my-5">
@@ -45,6 +85,8 @@ function Registrar() {
               type="password"
               placeholder="Tu password"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={password}
+              onChange={ e => setPassword(e.target.value)}
             />
           </div>
           <div className="my-5">
@@ -57,28 +99,30 @@ function Registrar() {
               type="password"
               placeholder="Repite tu password"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={repetirPassword}
+              onChange={ e => setRepetirPassword(e.target.value)}
             />
           </div>
           <input
-                        type="submit"
-                        value="Iniciar Sesion"
-                        className="bg-indigo-700 w-full py-3 px-10
+            type="submit"
+            value="Crear cuenta"
+            className="bg-indigo-700 w-full py-3 px-10
                     rounded-xl text-white uppercase font-bold
                     mt-5 hover:cursor-pointer
                     hover:bg-indigo-800 md:w-auto"
-                    />
-          </form>
-          <nav className="mt-10 lg:flex lg:justify-between">
-                    <Link
-                        className="block text-center my-5 text-gray-500"
-                        to="/">¿Ya tienes una cuenta? Inicia Sesion</Link>
-                        <Link
-                        className="block text-center my-5 text-gray-500"
-                        to="/olvide-password">Olvide mi password </Link>
-                    
-               </nav>
+          />
+        </form>
+        <nav className="mt-10 lg:flex lg:justify-between">
+          <Link
+            className="block text-center my-5 text-gray-500"
+            to="/">¿Ya tienes una cuenta? Inicia Sesion</Link>
+          <Link
+            className="block text-center my-5 text-gray-500"
+            to="/olvide-password">Olvide mi password </Link>
+
+        </nav>
       </div>
-      
+
 
     </>
 

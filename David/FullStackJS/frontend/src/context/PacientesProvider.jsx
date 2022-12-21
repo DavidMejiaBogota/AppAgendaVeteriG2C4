9cf3a87dc989080d140f7866eb0f9c59f357f7a5
DavidMejiaBogota <jose.david.mejia.meneses@gmail.com>
2022-12-21 12:20:38 -0500

    Actualizar pacientes en la bd desde el formulario de frontend

--- David/FullStackJS/frontend/src/context/PacientesProvider.jsx
+++ David/FullStackJS/frontend/src/context/PacientesProvider.jsx
@@ -32,28 +32,37 @@ export const PacientesProvider = ({children}) => {
     }, []);
 
     const guardarPaciente = async (paciente) => {
-        if(paciente.id) {
-            console.log('editando...')
-        } else {
-            console.log('nuevo')
-        }
-        return
 
-        try {
-            const token = localStorage.getItem('token')
-            const config = {
-                headers: {
-                    'contentType' : 'application/json',
-                    Authorization : `Bearer ${token}`
-                }
+        console.log(paciente)
+        
+        const token = localStorage.getItem('token')
+        const config = {
+            headers: {
+                'contentType' : 'application/json',
+                Authorization : `Bearer ${token}`
             }
-            const {data} = await clienteAxios.post('/pacientes', paciente, config)
-            const {createdAt, updatedAt, __v, ...pacienteAlmacenado} = data;
-
-            setPacientes([pacienteAlmacenado, ...pacientes]);
-        } catch (error) {
-            console.log(error.response.data.msg)
         }
+
+        if(paciente.id) {
+            try {
+                const {data} = await clienteAxios.put(`/pacientes/${paciente.id}`, paciente, config)
+                
+                const pacientesActualizado = pacientes.map(pacienteState => pacienteState._id === data._id ? data : pacienteState)
+
+                setPacientes(pacientesActualizado)
+            } catch (error) {
+                console.log(error)
+            }
+        } else {
+            try {
+                const {data} = await clienteAxios.post('/pacientes', paciente, config)
+                const {createdAt, updatedAt, __v, ...pacienteAlmacenado} = data;
+                setPacientes([pacienteAlmacenado, ...pacientes]);
+            } catch (error) {
+                console.log(error.response.data.msg)
+            }
+        }
+
     }
 
     const setEdicion = (paciente) => {

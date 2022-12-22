7620dc18213ea5c090168f8bd1b6916abe50ebe2
DavidMejiaBogota <jose.david.mejia.meneses@gmail.com>
2022-12-22 11:20:22 -0500

    cargar los pacientes del usuario/veterinario autenticado

--- David/FullStackJS/frontend/src/context/PacientesProvider.jsx
+++ David/FullStackJS/frontend/src/context/PacientesProvider.jsx
@@ -1,10 +1,12 @@
 import { createContext, useState, useEffect } from "react";
 import clienteAxios from "../config/axios";
+import useAuth from "../hooks/useAuth";
 
 const PacientesContext = createContext();
 export const PacientesProvider = ({children}) => {
     const [pacientes, setPacientes] = useState([]);
     const [paciente, setPaciente] = useState({});
+    const {auth} = useAuth();
 
     useEffect(() => {
         const obtenerPacientes = async () => {
@@ -27,7 +29,7 @@ export const PacientesProvider = ({children}) => {
             }
         }
         obtenerPacientes();
-    }, []);
+    }, [auth]);
 
     const guardarPaciente = async (paciente) => {
 

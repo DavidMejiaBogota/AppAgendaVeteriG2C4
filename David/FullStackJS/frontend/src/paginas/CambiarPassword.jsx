03e2c58471e5e0db7815e02976d11e2efceacac3
DavidMejiaBogota <jose.david.mejia.meneses@gmail.com>
2022-12-21 20:52:03 -0500

    Formulario para cambio de password

--- David/FullStackJS/frontend/src/paginas/CambiarPassword.jsx
+++ David/FullStackJS/frontend/src/paginas/CambiarPassword.jsx
@@ -1,6 +1,16 @@
-import AdminNav from '../components/AdminNav'
+import { useState } from 'react';
+import AdminNav from '../components/AdminNav';
+import Alerta from "../components/Alerta";
+
 
 const CambiarPassword = () => {
+
+    const [alerta, setAlerta] = useState({});
+    const handleSumbit = e => {
+        e.preventDefault();
+    }
+
+    const {msg} = alerta;
   return (
     <>
         <AdminNav />
@@ -9,6 +19,42 @@ const CambiarPassword = () => {
         <p className="text-xl my-5 text-center ">Modifica tu {''}
             <span className='text-indigo-600 font-bold '>Password aquí</span>
         </p>
+
+        <div className="flex justify-center">
+            <div className="w-full md:w-1/2 bg-white shadow rounded-lg p-5">
+                
+                {msg && <Alerta alerta={alerta} />}
+                <form
+                    onSubmit={handleSumbit}
+                >
+                    <div className="my-3 ">
+                        <label className="uppercase font-bold text-gray-600">Password Actual</label>
+                        <input
+                            type="text"
+                            className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
+                            name="nombre"
+                            placeholder='Escribe tu password actual'
+                        />
+                    </div>
+
+                    <div className="my-3 ">
+                        <label className="uppercase font-bold text-gray-600">Nuevo Password :</label>
+                        <input
+                            type="text"
+                            className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
+                            name="nombre"
+                            placeholder='Escribe tu nuevo password'
+                        />
+                    </div>
+
+                    <input
+                        type="submit"
+                        value="Actualizar Password"
+                        className="bg-indigo-700 hover:bg-indigo-500 px-10 py-3 font-bold text-white rounded-lg uppercase w-full mt-5"
+                    />
+                </form>
+            </div>
+        </div>
     </>
   );
 };

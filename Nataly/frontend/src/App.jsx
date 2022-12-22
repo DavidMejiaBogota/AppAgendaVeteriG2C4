e551cf1d579179042d52fdfac61b25ed4c6af446
natalyq1 <onatylili@gmail.com>
2022-12-22 16:12:44 -0500

    Saving patients working on frontend and backend

--- Nataly/frontend/src/App.jsx
+++ Nataly/frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthLayout from "./layout/AuthLayout";
 import RutaProtegida from "./layout/RutaProtegida";
 
@@ -7,36 +7,37 @@ import Login from "./paginas/Login";
 import NuevoPassword from "./paginas/NuevoPassword";
 import OlvidePassword from "./paginas/OlvidePassword";
 import Registrar from "./paginas/Registrar";
+import AdministrarPacientes from "./paginas/AdministrarPacientes";
 
 import { AuthProvider } from "./context/AuthProvider";
-
-import { AdministrarPacientes } from "./paginas/AdministrarPacientes";
+import { PacientesProvider } from "./context/PacientesProvider";
 
 function App() {
- //console.log(import.meta.env.VITE_BACKEND_URL);
+  //console.log(import.meta.env.VITE_BACKEND_URL);
   return (
-    
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path="/" element={<AuthLayout/>}>
-            <Route index element={<Login/>}/>
-            <Route path="registrar" element={<Registrar/>}/>
-            <Route path="olvide-password" element={<OlvidePassword/>}/>
-            <Route path="olvide-password/:token" element={<NuevoPassword/>}/>
-            <Route path="confirmar/:id" element={<ConfirmarCuenta/>}/>
-          </Route>
+        <PacientesProvider>
+          <Routes>
+            <Route path="/" element={<AuthLayout />}>
+              <Route index element={<Login />} />
+              <Route path="registrar" element={<Registrar />} />
+              <Route path="olvide-password" element={<OlvidePassword />} />
+              <Route
+                path="olvide-password/:token"
+                element={<NuevoPassword />}
+              />
+              <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
+            </Route>
 
-          <Route path="/admin" element={<RutaProtegida/>}>
-            <Route index element={<AdministrarPacientes/>} />
-
-          </Route>
-
-        </Routes>
+            <Route path="/admin" element={<RutaProtegida />}>
+              <Route index element={<AdministrarPacientes />} />
+            </Route>
+          </Routes>
+        </PacientesProvider>
       </AuthProvider>
     </BrowserRouter>
-    
-  )
+  );
 }
 
-export default App
+export default App;
--- Nataly/frontend/src/components/Formulario.jsx
+++ Nataly/frontend/src/components/Formulario.jsx
@@ -1,7 +1,132 @@
-import React from 'react'
+import { useState } from "react"
+import Alerta from "./Alerta"
+import usePacientes from "../hooks/usePacientes"
 
-export const Formulario = () => {
+ const Formulario = () => {
+    const [nombre, setNombre] = useState('')
+    const [propietario, setPropietario] = useState('')
+    const [email, setEmail] = useState('')
+    const [fecha, setFecha] = useState('')
+    const [sintomas, setSintomas] = useState('')
+
+    const [alerta, setAlerta] = useState({})
+
+    const { guardarPaciente } =usePacientes()
+    
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        //validar form
+        if ([nombre, propietario,email,fecha,sintomas].includes('')) {
+            setAlerta({
+                msg: "Todos los campos son obligatorios",
+                error: true
+            })
+            return
+        }
+        setAlerta({})
+        guardarPaciente({nombre, propietario,email,fecha,sintomas})
+    }
+    const { msg } = alerta
+
   return (
-    <div>Formulario</div>
+    <>
+
+    <p className=" text-lg text-center mb-10">
+        Añade tus pacientes y {' '}
+        <span className="text-indigo-600 font-bold">Administralos</span>
+    </p>
+
+    <form
+    className="bg-white py-10 px-5 mb-10 lg:mb-5 shadow-md rounded-md"
+    onSubmit={handleSubmit}
+    >
+        <div className="mb-5">
+            <label 
+            htmlFor="nombre"
+            className="text-gray-700 uppercase font-bold"
+            >Nombre Mascota</label>
+            <input 
+            id="mascota"
+            type="text"
+            placeholder="Nombre de la mascota"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+            value={nombre}
+            onChange={e => setNombre(e.target.value)}
+            />
+        </div>
+
+        <div className="mb-5">
+            <label 
+            htmlFor="propietario"
+            className="text-gray-700 uppercase font-bold"
+            >Nombre Propietario</label>
+            <input 
+            id="propietario"
+            type="text"
+            placeholder="Nombre del propietario"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={propietario}
+            onChange={e => setPropietario(e.target.value)}
+            />
+        </div>
+
+        <div className="mb-5">
+            <label 
+            htmlFor="email"
+            className="text-gray-700 uppercase font-bold"
+            >Email</label>
+            <input 
+            id="email"
+            type="email"
+            placeholder="Email del propietario"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            />
+        </div>
+
+        <div className="mb-5">
+            <label 
+            htmlFor="fecha"
+            className="text-gray-700 uppercase font-bold"
+            >Fecha de Alta</label>
+            <input 
+            id="fecha"
+            type="date"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={fecha}
+            onChange={e => setFecha(e.target.value)}
+            />
+        </div>
+
+        <div className="mb-5">
+            <label 
+            htmlFor="sintomas"
+            className="text-gray-700 uppercase font-bold"
+            >Síntomas</label>
+            <textarea
+            id="sintomas"
+            placeholder="Describe los síntomas"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={sintomas}
+            onChange={e => setSintomas(e.target.value)}
+            />
+        </div>
+
+        <input 
+        type="submit"
+        className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+        value="Agregar paciente"
+        />
+
+    </form>
+
+    {msg && 
+    <Alerta  alerta ={alerta}/>}
+    
+    </>
   )
 }
+export default Formulario
